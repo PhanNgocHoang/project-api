d0467e70f6cb28011217f6d2cee082089b5c4ae4
Hoang Phan Ngoc <hoang.phan.intern@ntq-solution.com.vn>
2021-01-21 15:53:55 +0700

    upload file

--- src/routers/upload.js
+++ src/routers/upload.js
@@ -1,8 +1,9 @@
 const routers = require('express').Router()
 const multer = require('multer')
 const fs = require('fs')
+const createError = require('http-errors')
 const cloudinary = require('cloudinary').v2
-const  {cloudinaryConfig} = require('../config/config')
+const { cloudinaryConfig } = require('../config/config')
 
 cloudinary.config({
     cloud_name: cloudinaryConfig.cloud_name,
@@ -11,28 +12,48 @@ cloudinary.config({
 })
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) =>{
+    destination: (req, file, cb) => {
         return cb(null, '/tmp/')
     }
 })
-const fileFilterImages = (req, file, cb) =>{
-    //only upload files jpg or png
-    if(file.mimetype === 'image/jpeg' ||file.mimetype === 'image/png')
-    {return cb(null, true)}
-    else{return cb(null, false)}
+const fileFilterPDF = (req, file, cb) => {
+    if (file.mimetype === 'application/pdf') { return cb(null, true) }
+    else { return cb(createError(400, "Please only upload PDF file"), false) }
 }
-const upload = multer({storage: storage, limits: {fileSize:1024*1024*10}, fileFilter:fileFilterImages})
-routers.post('/images',upload.array('images', 10) , async (req, res, next) => {
-    let images = [];
-    for(const image of req.files){
-        const imageUrls = await cloudinary.uploader.upload(image.path, {folder: 'images'}, (err, result)=>{
-            if(err) throw new Error(err)
-            return result
-        })
-        images.push(imageUrls.url)
-        fs.unlinkSync(image.path)
+const fileFilterImages = (req, file, cb) => {
+    //only upload files jpg or png
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') { return cb(null, true) }
+    else { return cb(createError(400, "Please only upload images jpg or png"), false) }
+}
+const uploadImage = multer({ storage: storage, limits: { fileSize: 1024 * 1024 * 10 }, fileFilter: fileFilterImages })
+const uploadPDF = multer({ storage: storage, limits: { fileSize: 1024 * 1024 * 10 }, fileFilter: fileFilterPDF })
+routers.post('/images', uploadImage.array('images', 10), async (req, res, next) => {
+    try {
+        let images = [];
+        for (const image of req.files) {
+            const imageUrls = await cloudinary.uploader.upload(image.path, { folder: 'images' }, (err, result) => {
+                if (err) throw new Error(err)
+                return result
+            })
+            images.push(imageUrls.url)
+            fs.unlinkSync(image.path)
+        }
+        return res.status(200).json({ images: images })
+    } catch (error) {
+        next(error)
     }
-    return res.status(200).json({images: images})
+})
+routers.post('/pdf', uploadPDF.single('doc'), async (req, res, next) => {
+    try {
+            const documentUrls = await cloudinary.uploader.upload(req.file.path, { folder: 'document' }, (err, result) => {
+                if (err) throw new Error(err)
+                return result
+            })
+            fs.unlinkSync(req.file.path)
+            return res.status(200).json({ documentUrls: documentUrls.url })
+    } catch (error) {
+        next(createError(500, error))
+    }
 })
 
 
