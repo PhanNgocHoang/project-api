bf4de86f3c7ceb25c4b33b0334e84835b7788c3c
Hoang Phan Ngoc <hoang.phan.intern@ntq-solution.com.vn>
2021-01-27 14:31:08 +0700

    update author

--- src/services/admin/admin.services.author.js
+++ src/services/admin/admin.services.author.js
@@ -13,7 +13,7 @@ module.exports.deleteAuthor = (authorId) => {
 module.exports.getAuthors = async (page, perPage, searchKey) => {
     const totalItems = await Authors.countDocuments()
     const skip = (page - 1) * perPage
-    const authors = await Authors.find({ authorName: { $regex: searchKey, $options: 'mis' }, dob: { $regex: searchKey, $options: 'mis' } }).skip(skip).limit(perPage)
+    const authors = await Authors.find({ authorName: { $regex: searchKey, $options: 'mis' } }).skip(skip).limit(perPage)
     return { data: authors, currentPage: page, totalItems: totalItems, perPage: perPage }
 }
 module.exports.findAuthorByName = (authorName) => {
