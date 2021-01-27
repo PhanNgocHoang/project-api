2084144b290ef121522ceb3aace89eb6fcb2effa
Hoang Phan Ngoc <hoang.phan.intern@ntq-solution.com.vn>
2021-01-27 20:51:16 +0700

    update get all

--- src/models/books.model.js
+++ src/models/books.model.js
@@ -1,16 +1,26 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
 const BookSchema = new Schema({
-    book_name: { type: String, required: true },
-    author: [{ type: mongoose.Schema.Types.ObjectId, required: true, ref: "authors" }],
-    type_book: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "type_book" },
-    publisher: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "publisher" },
-    status: { type: Boolean, required: true, default: true },
-    description: { type: String, required: true },
-    images: { type: Array, required: true },
-    file: { type: String, required: true }
-})
-BookSchema.index({ '$**': 'text' })
-const Book = mongoose.model('books', BookSchema)
-module.exports = Book
+  book_name: { type: String, required: true },
+  author: [
+    { type: mongoose.Schema.Types.ObjectId, required: true, ref: "authors" },
+  ],
+  book_type: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "type_book",
+  },
+  publisher: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "publisher",
+  },
+  status: { type: Boolean, required: true, default: true },
+  description: { type: String, required: true },
+  images: { type: Array, required: true },
+  file: { type: String, required: true },
+});
+BookSchema.index({ "$**": "text" });
+const Book = mongoose.model("books", BookSchema);
+module.exports = Book;
