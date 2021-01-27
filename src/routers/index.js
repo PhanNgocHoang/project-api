d7fadbc256f0cf2a624ef91f0e8acadfa7781caf
Hoang Phan Ngoc <hoang.phan.intern@ntq-solution.com.vn>
2021-01-27 21:09:48 +0700

    update get all

--- src/routers/index.js
+++ src/routers/index.js
@@ -4,10 +4,12 @@ const typebookRouter = require("./typebook.router");
 const upload = require("./upload");
 const authors = require("./author.router");
 const publisher = require("./publisher.router");
+const books = require("./books.router");
 
 routers.use("/auth", authRouter);
 routers.use("/typebook", typebookRouter);
 routers.use("/upload", upload);
 routers.use("/author", authors);
 routers.use("/publisher", publisher);
+routers.use("/books", books);
 module.exports = routers;
