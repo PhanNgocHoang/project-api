696c8251727f6bd75de7c3078b2b46816fce305b
Hoang Phan Ngoc <hoang.phan.intern@ntq-solution.com.vn>
2021-01-27 19:49:16 +0700

    update books

--- src/models/books.model.js
+++ src/models/books.model.js
@@ -5,7 +5,8 @@ const BookSchema = new Schema({
     book_name: { type: String, required: true },
     author: [{ type: mongoose.Schema.Types.ObjectId, required: true, ref: "authors" }],
     type_book: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "type_book" },
-    publisher: { type: mongoose.Schema.Types.ObjectID, required: true, ref: "publisher" },
+    publisher: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "publisher" },
+    status: { type: Boolean, required: true, default: true },
     description: { type: String, required: true },
     images: { type: Array, required: true },
     file: { type: String, required: true }
