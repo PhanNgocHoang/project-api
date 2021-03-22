8bc234ef8ea86a1f9a75fa39439aa20c5712752f
Hoang Phan Ngoc <hoang.phan@ntq-solution.com.vn>
2021-03-22 17:04:22 +0700

    update

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,7 +1,5 @@
 const admin = require("firebase-admin");
-const {
-  firebase,
-} = require("./e-library-705ec-firebase-adminsdk-rw603-c244dd2e2b.json");
+const firebase = require("./e-library-705ec-firebase-adminsdk-rw603-c244dd2e2b.json");
 
 // Initialize firebase admin SDK
 admin.initializeApp({
