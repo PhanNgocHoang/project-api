3d093ec3505affb312ce20fc4899c2296407f4da
Hoang Phan Ngoc <hoang.phan@ntq-solution.com.vn>
2021-03-26 11:42:47 +0700

    update cron

--- index.js
+++ index.js
@@ -8,8 +8,10 @@ const routers = require("./src/routers/index");
 const mongoose = require("mongoose");
 const passport = require("passport");
 const cron = require("node-cron");
+const gridMail = require("@sendgrid/mail");
 const { changeOrderStatus } = require("./src/services/customer/order.services");
 const PORT = process.env.PORT || 4000;
+gridMail.setApiKey(process.env.MAIL_KEY);
 
 mongoose.connect(
   "mongodb+srv://admin:Admin123@@cluster0.9m6f6.mongodb.net/eLibrary?retryWrites=true&w=majority",
@@ -27,6 +29,26 @@ mongoose.connect(
 
 cron.schedule("0 0,6,12,18,23 * * *", async () => {
   await changeOrderStatus();
+  gridMail
+    .send({
+      to: {
+        email: "hoangpn.dev@gmail.com",
+      },
+      templateId: "d-e4a7316604634d2cb081b67ede3f94ca",
+      dynamicTemplateData: {
+        displayName: new Date().toISOString(),
+      },
+      from: {
+        email: "hoangpn.dev@gmail.com",
+        name: "Admin",
+      },
+    })
+    .then(() => {
+      console.log("run cron");
+    })
+    .catch((err) => {
+      console.log("run cron err");
+    });
 });
 app.use(passport.initialize());
 app.use(passport.session());
